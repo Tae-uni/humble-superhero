2124c8c25ed5881d2f5ea77a15c0da839b3a90d3
taekwon <universe134679@gmail.com>
2025-02-03 02:03:37 +0900

    :sparkles: add validation middleware with Zod

---
+++ backend/src/middlewares/validateSuperhero.ts
@@ -0,0 +1,27 @@
+import { NextFunction, Request, Response } from "express";
+import { z } from "zod";
+
+// Superhero validation schema with Zod
+const superheroSchema = z.object({
+  name: z.string()
+    .min(2, "Name must be at least 2 characters")
+    .max(20, "Name must be at most 20 characters or less"),
+  superpower: z.string()
+    .min(2, "The superpower must be at least 2 characters")
+    .max(20, "The superpower must be 20 characters or less"),
+  humilityScore: z.number()
+    .int("The humility score must be an integer")
+    .min(1, "The humility score cannot be lower than 1")
+    .max(10, "The Humility score cannot be higher than 10"),
+});
+
+// Validate the superhero data middleware
+export const validateSuperhero = (req: Request, res: Response, next: NextFunction) => {
+  const validationResult = superheroSchema.safeParse(req.body);
+
+  if (validationResult.success) {
+    next();
+  } else {
+    next(validationResult.error);
+  }
+};
