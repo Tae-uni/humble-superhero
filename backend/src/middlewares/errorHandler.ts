016bb7e2f19e7a53e7f069a6849b8c6829eceede
taekwon <universe134679@gmail.com>
2025-02-03 02:26:29 +0900

    :recycle: improve global errorHandler for better readability / efficiency

--- backend/src/middlewares/errorHandler.ts
+++ backend/src/middlewares/errorHandler.ts
@@ -1,28 +1,27 @@
 import { Request, Response, NextFunction } from "express";
 import { ZodError } from "zod";
 
-// Error handler middleware
-export const errorHandler = (error: any, req: Request, res: Response, next: NextFunction) => {
-  const statusCode = error instanceof ZodError ? 400 : error.status || 500;
-  let message = error.message || "Internal server error";
-
+// Global Error handler middleware
+export const errorHandler = (
+  error: any,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   if (error instanceof ZodError) {
-    message = "Invalid request data. Please check your data.";
-
-    return res.status(statusCode).json({
-      error: {
-        message,
-        details: error.errors.map(e => ({
-          field: e.path.join("."),
-          issue: e.message,
-        })),
-      },
+    return res.status(400).json({
+      error:
+        "Invalid request data. Please check your data.",
+      details: error.errors.map(e => ({
+        field: e.path.join("."),
+        issue: e.message,
+      })),
     });
   }
 
-  res.status(statusCode).json({
+  res.status(error.status || 500).json({
     error: {
-      message,
+      message: error.message || "Internal Server Error",
     },
   });
 };
